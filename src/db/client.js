8ba50bf1ed0e82be5c5c82b824d0efa836bb4b34
AlexanderToT <1328457151@qq.com>
2025-05-19 16:29:33 +0800

    连接线上数据库

--- src/db/client.js
+++ src/db/client.js
@@ -1,13 +1,13 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema.js';
-import { getDatabaseUrl, DATABASE } from '../config/index.js';
+import { getDatabaseUrl } from '../config/index.js';
 
 // 创建数据库连接
 const connectionString = getDatabaseUrl();
 const client = postgres(connectionString, {
-    max: DATABASE.MAX_CONNECTIONS,
-    idle_timeout: DATABASE.IDLE_TIMEOUT,
+    max: 10,
+    idle_timeout: 30000,
     connect_timeout: 10,
     connection: {
         timezone: 'Asia/Shanghai'
